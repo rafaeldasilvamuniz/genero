db37b9a2ab32efee80a5b8ebf9962b799db02a63
rafaeldasilvamuniz <86721430+rafaeldasilvamuniz@users.noreply.github.com>
2023-08-25 21:33:59 -0300

    Add files via upload

---
+++ Pessoa.java
@@ -0,0 +1,21 @@
+public class Pessoa {
+
+   private String nome;
+   private String genero;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+}
---
+++ genero.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.ArrayList;
+public class genero {
+
+    public static void main(String args[]) {
+
+        //Calculo cal = new Calculo();
+        Pessoa pe  = new Pessoa();
+        Scanner sc = new Scanner(System.in);
+        java.util.List<Pessoa> h = new ArrayList<>();
+        java.util.List<Pessoa> m = new ArrayList<>();
+        java.util.List<Pessoa> o = new ArrayList<>();
+        Set<String> lista = new HashSet<String>();
+
+       while (true) {
+
+
+           System.out.println("Digite o seu nome completo: ");
+           //Set<String> lista = new HashSet<String>();
+           String no = sc.next();
+           if(no.equalsIgnoreCase("sair")){
+               break;
+           }
+           pe.setNome(no);
+           lista.add(pe.getNome());
+       }
+        System.out.println("Digite o seu gênero: ((F)Feminino,(M)Masculino,(O)Outro: ");
+        java.util.List<String> gen = new ArrayList<String>();
+        String ge = sc.next();
+        pe.setGenero(ge);
+        gen.add(pe.getGenero());
+
+        if(ge.equalsIgnoreCase("M")){
+            h.add(pe);
+
+        } else if(ge.equalsIgnoreCase("F")){
+            m.add(pe);
+        } else if(ge.equalsIgnoreCase("O")){
+            o.add(pe);
+        }
+        lista.forEach(list ->  System.out.println("Obrigado " + lista + " por informar o seu nome, o seu gênero e a sua orientação sexual  " + gen + " " ));
+
+    }
+}
+
+
+
